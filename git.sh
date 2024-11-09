35b6cf6bca5e80179460e13ae3b4e2058137405c
zhao ding <demissiezhao@gmail.com>
2024-11-09 11:46:12 +0800

    1

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 git filter-branch --env-filter '
 OLD_EMAIL="dingzhao@dingzhaodeMacBook-Pro.local"
-CORRECT_NAME="ZhaoDing"
+CORRECT_NAME="zhao ding"
 CORRECT_EMAIL="demissiezhao@gmail.com"
 if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
 then
